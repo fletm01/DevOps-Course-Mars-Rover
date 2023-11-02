6817cba26e94a73412cfeaacfe7c27d48af38ebe
Mike Fletcher <mike11@mike11.karoo.co.uk>
2023-11-02 15:34:24 +0000

    Added image of the day

--- app/app.py
+++ app/app.py
@@ -1,11 +1,16 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
-
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', landing_image='https://via.placeholder.com/500')
+    key = os.getenv('API_KEY')
+    response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={key}')
+    Result = response.json()
+    image_url = Result ['url']
+    return render_template('index.html', landing_image= (image_url))
 
 @app.route('/mars')
 def mars():
