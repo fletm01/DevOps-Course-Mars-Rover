83b79ef885742f7dd5604a128eb7e72c4c872617
Mike Fletcher <mike11@mike11.karoo.co.uk>
2023-11-03 15:09:34 +0000

    Added Title and Explanation

--- app/app.py
+++ app/app.py
@@ -10,7 +10,9 @@ def index():
     response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={key}')
     Result = response.json()
     image_url = Result ['url']
-    return render_template('index.html', landing_image= (image_url))
+    image_title = Result ['title']
+    explanation = Result ['explanation']
+    return render_template('index.html', landing_image= (image_url), image_title= (image_title), explanation= (explanation))
 
 @app.route('/mars')
 def mars():
